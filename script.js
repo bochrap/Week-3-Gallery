ab379da61125d4c95a35397d3f888588cd79b9cc
bochrap <131978926+bochrap@users.noreply.github.com>
2023-12-15 10:47:38 +0000

    displayed image covering whole viewport

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const thumbnails = document.querySelectorAll(".thumbnails img");
 let displayedImg = document.querySelector(".displayed");
+let ariaContainer = document.getElementById("info");
 
 // scriptObject.src = URL
 
@@ -51,6 +52,7 @@ function setmiddle(index) {
 
   thumbnails[2].src = srcArray[index];
   console.log(`Middle image is ${srcArray[index]}`);
+  ariaContainer.textContent = `Currently displaying ${srcArray[index]}`;
 
   thumbnails[3].src = srcArray[plusIndex];
   thumbnails[4].src = srcArray[plus2Index];
